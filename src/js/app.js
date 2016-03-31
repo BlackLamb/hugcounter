d0a044d3aec44a7ddc16b359ab4f02faab4df05d
BlackLamb <blamb001@gmail.com>
2016-03-31 09:49:20 -0700

    Synchronized settings with settings page
    todo: sync local storage with watch storage

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,7 @@
+var totalHugsNum = window.localStorage.getItem('TotalHugsNum') ? window.localStorage.getItem('TotalHugsNum') : 1000;
+
 Pebble.addEventListener('showConfiguration', function() {
-	var url = 'https://dl.dropboxusercontent.com/spa/id55snvxb28xi4a/hugcounter/index.html';
+	var url = 'https://dl.dropboxusercontent.com/spa/id55snvxb28xi4a/hugcounter/index.html?totalhugs=' + totalHugsNum;
 	
 	Pebble.openURL(url);
 });
@@ -11,6 +13,8 @@ Pebble.addEventListener('webviewclosed', function(e) {
 		'AppTotalHugsNum': configData.total_hugs_num,
 		'AppResetOnSave': configData.reset_on_save
 	};
+	totalHugsNum = configData.total_hugs_num
+	window.localStorage.setItem('TotalHugsNum', totalHugsNum);
 	//console.log(JSON.stringify(configData));
 	
 	Pebble.sendAppMessage(dict, function() {
