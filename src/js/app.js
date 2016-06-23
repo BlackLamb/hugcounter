6bb4d1d09329b7eae0c1991c49079076b0370e20
BlackLamb <blamb001@gmail.com>
2016-06-22 19:03:54 -0700

    Added clay and config file
    TODO: sync settings, may need new clay version or custom handler

--- src/js/app.js
+++ src/js/app.js
@@ -1,29 +1,33 @@
-var totalHugsNum = localStorage.getItem('TotalHugsNum') ? localStorage.getItem('TotalHugsNum') : 1000;
-var showSecondsBol = localStorage.getItem('ShowSeconds') ? localStorage.getItem('ShowSeconds') : 1;
+var Clay = require('pebble-clay');
+var clayConfig = require('./config');
+var clay = new Clay(clayConfig);
 
-Pebble.addEventListener('showConfiguration', function() {
-	var url = 'https://dl.dropboxusercontent.com/spa/id55snvxb28xi4a/hugcounter/index.html?totalhugs=' + totalHugsNum + '&showseconds=' + showSecondsBol;
-	
-	Pebble.openURL(url);
-});
+//var totalHugsNum = localStorage.getItem('TotalHugsNum') ? localStorage.getItem('TotalHugsNum') : 1000;
+//var showSecondsBol = localStorage.getItem('ShowSeconds') ? localStorage.getItem('ShowSeconds') : 1;
 
-Pebble.addEventListener('webviewclosed', function(e) {
-	var configData = JSON.parse(decodeURIComponent(e.response));
-	
-	var dict = {
-		'AppTotalHugsNum': configData.total_hugs_num,
-		'AppResetOnSave': configData.reset_on_save,
-		'AppShowSeconds': configData.show_seconds
-	};
-	totalHugsNum = configData.total_hugs_num;
-	localStorage.setItem('TotalHugsNum', totalHugsNum);
-	showSecondsBol = configData.show_seconds;
-	localStorage.setItem('ShowSeconds', showSecondsBol);
-	//console.log(JSON.stringify(configData));
-	
-	Pebble.sendAppMessage(dict, function() {
-		console.log('Config data sent successfully!');
-	}, function(e) {
-		console.log('Error sending config data!');
-	});
-});
+//Pebble.addEventListener('showConfiguration', function() {
+//	var url = 'https://dl.dropboxusercontent.com/spa/id55snvxb28xi4a/hugcounter/index.html?totalhugs=' + totalHugsNum + '&showseconds=' + showSecondsBol;
+//	
+//	Pebble.openURL(url);
+//});
+
+//Pebble.addEventListener('webviewclosed', function(e) {
+//	var configData = JSON.parse(decodeURIComponent(e.response));
+//	
+//	var dict = {
+//		'AppTotalHugsNum': configData.total_hugs_num,
+//		'AppResetOnSave': configData.reset_on_save,
+//		'AppShowSeconds': configData.show_seconds
+//	};
+//	totalHugsNum = configData.total_hugs_num;
+//	localStorage.setItem('TotalHugsNum', totalHugsNum);
+//	showSecondsBol = configData.show_seconds;
+//	localStorage.setItem('ShowSeconds', showSecondsBol);
+//	//console.log(JSON.stringify(configData));
+//	
+//	Pebble.sendAppMessage(dict, function() {
+//		console.log('Config data sent successfully!');
+//	}, function(e) {
+//		console.log('Error sending config data!');
+//	});
+//});
